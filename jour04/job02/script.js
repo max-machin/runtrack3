f200472e470a0452a89392724a4cac097ad2193d
max-machin <machin.max@lapalteforme.io>
2022-03-17 13:31:34 +0100

    Ajout job02

--- jour04/job02/script.js
+++ jour04/job02/script.js
@@ -1,21 +1,17 @@
 window.addEventListener("DOMContentLoaded", (event) => {
 
     const obj = {name: "La Plateforme_", address: "8 rued'hozier", city: "Marseille", nb_staff: "11", creation:"2019"}
-    console.log(obj)
-    const myjson = JSON.stringify(obj)
-    let name = obj.name
-
+    
+    let critere = "name";
    
-    function jsonValueKey(obj, critere) {
-        Object.keys(obj).forEach(function(key) {
-            if ( key == critere){
-                console.log(obj[key] )
-                return (obj[key])
-            }
-          })
+    function jsonValueKey(obj, key) {
+    
+        if ( obj[key] != undefined ) {
+            console.log(obj[key])
+        }
     }
 
-    let critere = "address";
+    
 
     jsonValueKey(obj, critere);
 });
