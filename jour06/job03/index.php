c592fb8e60d2fd1ac586506bef9eb3c826f7e2d1
max-machin <machin.max@lapalteforme.io>
2022-03-21 15:31:38 +0100

    Job 04/ jour06

---
+++ jour06/job03/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Jour 06</title>
+</head>
+<body>
+
+    <header>
+        <nav class="navbar-desktop">
+            <div>
+                <a href=""> Accueil </a>
+                <a href=""> Présentation </a>
+                <a href=""> Contact </a>
+            </div>
+        </nav>
+        <nav class="navbar-mobile">
+            <label for="toggle">☰</label>
+            <input type="checkbox" id="toggle">
+            <div class="main_pages">
+                <a href="#">Accueil</a>
+                <a href="#">Actualité</a>
+                <a href="#">Contact</a>
+            </div>
+        </nav>
+    </header>
+    <div class="grid">
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+        <section class="section"></section>
+    </div>
+</body>
+</html>
