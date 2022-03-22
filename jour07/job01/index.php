be1421535416d6e5d2be82abe6b7a50b74aca4c2
max-machin <machin.max@lapalteforme.io>
2022-03-22 17:21:07 +0100

    Ajout jour jour07 job01

---
+++ jour07/job01/index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Jour 07</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light ">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">LPTF</a>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                        <li class="nav-item"><a href="" class="nav-link active">Accueil</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">Units</a></li>
+                        <li class="nav-item"><a href="" class="nav-link">Jobs</a></li>
+                        <li class="nav-item"><a href="" class="nav-link disabled">Skills</a></li>
+                    </ul>
+                </div>
+            </div>
+
+        </nav>
+    </header>
+    <main class="main">
+        <h1 class="display-5 text-center">LaPlateforme_</h1>
+        <div class="container-fluid">
+            <div class="row m-0 d-flex">
+                <div class="col-2">
+                    <div class="card">
+                        <img src="papillon.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Un papillon</h5>
+                            <p class="card-text">Un papillon, c'est un peu comme une chenille, mais avec des ailes.<br/> Ne pas ingérer.</p>
+                            <a href="#" class="btn btn-primary">Commander votre propre papillon</a>
+                        </div>
+                    </div>
+                </div>
+                
+                    <div class="jumbotron col pt-5 ps-4">
+                            <h1 class="display-5">Bonjour, monde!</h2>
+                            <p class="lead">Il existe plusieurs visions du terme : <br>
+                                Le monde est la matière, l'espace et les phénomènes qui nous sont accessibles par les sens, l'expérience ou la raison. <br>
+                                Le sens le plus courant désigne notre planète, la Terre, avec ses habitants et son environnement plus ou moins naturel.
+                            </p>
+                            <hr class="my-4">
+                            <p>Le sens étendu désigne l'univers dans son ensemble</p>
+                            <div class="d-flex">
+                                <a class="btn btn-danger btn-lg" href="#" role="button">Rebooter le monde</a>
+                                <div class="spinner-border text-info ms-2   " role="status">
+                                    <span class="sr-only"></span>
+                                </div>
+                                </p>
+                            </div>
+                            
+                            <ul class="pagination d-flex justify-content-end">
+                                <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                </a>
+                                </li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                </a>
+                                </li>
+                            </ul>
+
+                            
+                    </div>
+               
+                <div class="col-2" style="width: 10rem;">
+                    <ul class="list-group">
+                        <li class="list-group-item">Limbes</li>
+                        <li class="list-group-item">Luxures</li>
+                        <li class="list-group-item">Gourmandise</li>
+                        <li class="list-group-item">Avarice</li>
+                        <li class="list-group-item">Colere</li>
+                        <li class="list-group-item">Heresie</li>
+                        <li class="list-group-item">Violence</li>
+                        <li class="list-group-item">Ruse et Tromperie</li>
+                        <li class="list-group-item">Trahison</li>
+                        <li class="list-group-item">Internet Explorer</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="progress">
+            <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+        </div>
+    </main>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
